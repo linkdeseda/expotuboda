4d7410d4e1bd333feccc09519e78ec54c9fed958
Carlos Deseda <carlosdeseda@gmail.com>
2025-03-23 11:06:11 -0600

    se agrega position fixed al headeR

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="bg-brown text-white">
+    <header className="bg-brown text-white fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link to="/" className="flex items-center">
           <img src={logo} alt="Expo Tu boda" className={`h-12 ${styles.logoHeader}`} />
